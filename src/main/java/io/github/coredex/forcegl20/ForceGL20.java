68a878bf7faa64f0672f4848cba850c562e062af
Coredex <73810221+coredex-source@users.noreply.github.com>
2024-12-16 22:23:03 +0530

    Supposedly increasing the performance ?

--- src/main/java/io/github/coredex/forcegl20/ForceGL20.java
+++ src/main/java/io/github/coredex/forcegl20/ForceGL20.java
@@ -1,7 +1,6 @@
 package io.github.coredex.forcegl20;
 
 import com.google.common.collect.ImmutableMap;
-
 import io.github.coredex.forcegl20.config.ForceGL20Config;
 import io.github.coredex.forcegl20.override.HintOverride;
 import io.github.coredex.forcegl20.override.OverrideType;
@@ -11,8 +10,7 @@ import org.slf4j.LoggerFactory;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
-import java.util.Arrays;
-import java.util.List;
+import java.util.Set;
 
 public class ForceGL20 {
 
@@ -21,7 +19,7 @@ public class ForceGL20 {
     public static final ImmutableMap<Integer, HintOverride> GLFW_OVERRIDE_VALUES;
     public static final ImmutableMap<Integer, String> GLFW_HINT_NAMES;
 
-    private static final int[] GLFW_HINT_CODES = new int[] {
+    private static final Set<Integer> GLFW_HINT_CODES = Set.of(
             0x00020001, 0x00020002, 0x00020003, 0x00020004, 0x00020005, 0x00020006,
             0x00020007, 0x00020008, 0x00020009, 0x0002000A, 0x0002000B, 0x0002000C,
             0x00021001, 0x00021002, 0x00021003, 0x00021004, 0x00021005, 0x00021006,
@@ -30,10 +28,14 @@ public class ForceGL20 {
             0x00022003, 0x00022004, 0x00022005, 0x00022006, 0x00022007, 0x00022008,
             0x00022009, 0x0002200A, 0x0002200B, 0x0002200C, 0x00023001, 0x00023002,
             0x00023003, 0x00024001, 0x00024002
-    };
+    );
 
     static {
-        // Window hint values to override
+        GLFW_OVERRIDE_VALUES = createGlfwOverrideValues();
+        GLFW_HINT_NAMES = createGlfwHintNames();
+    }
+
+    private static ImmutableMap<Integer, HintOverride> createGlfwOverrideValues() {
         ImmutableMap.Builder<Integer, HintOverride> overrideBuilder = ImmutableMap.builder();
 
         int contextVersionMajor = ForceGL20Config.CONFIG.instance().contextVersionMajor;
@@ -42,28 +44,37 @@ public class ForceGL20 {
         overrideBuilder.put(GLFW.GLFW_OPENGL_PROFILE, new HintOverride(OverrideType.SET_VALUE, 0));
         overrideBuilder.put(GLFW.GLFW_OPENGL_FORWARD_COMPAT, HintOverride.DO_NOT_SET);
 
-        GLFW_OVERRIDE_VALUES = overrideBuilder.build();
+        return overrideBuilder.build();
+    }
 
-        // Make a list of Window hint names for logging
+    private static ImmutableMap<Integer, String> createGlfwHintNames() {
         ImmutableMap.Builder<Integer, String> nameBuilder = ImmutableMap.builder();
-        List<Integer> codes = Arrays.stream(GLFW_HINT_CODES).boxed().toList();
 
-        for (Field field : GLFW.class.getDeclaredFields()) {
-            if (field.getName().equals("GLFW_OPENGL_DEBUG_CONTEXT")) { // Alias for earlier versions, ignore it
+        Field[] fields = GLFW.class.getDeclaredFields();
+        for (Field field : fields) {
+            if (!Modifier.isPublic(field.getModifiers()) || !Modifier.isFinal(field.getModifiers())) {
                 continue;
             }
-            if (field.getType() == int.class && Modifier.isPublic(field.getModifiers()) && Modifier.isFinal(field.getModifiers())) {
-                try {
-                    int code = (int) field.get(null);
-                    if (codes.contains(code)) {
-                        nameBuilder.put(code, field.getName());
-                    }
-                } catch (IllegalAccessException e) {
-                    LOGGER.error("Failed to get field value for GLFW." + field.getName());
+
+            if (field.getType() != int.class) {
+                continue;
+            }
+
+            String fieldName = field.getName();
+            if (fieldName.equals("GLFW_OPENGL_DEBUG_CONTEXT")) {
+                continue;
+            }
+
+            try {
+                int code = field.getInt(null);
+                if (GLFW_HINT_CODES.contains(code)) {
+                    nameBuilder.put(code, fieldName);
                 }
+            } catch (IllegalAccessException e) {
+                LOGGER.error("Failed to access GLFW field: {}", fieldName, e);
             }
         }
-
-        GLFW_HINT_NAMES = nameBuilder.build();
+        
+        return nameBuilder.build();
     }
 }
