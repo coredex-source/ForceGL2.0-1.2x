1bfce52c4436820a2a1ad109d8ef5b461d0a7aa4
Coredex <73810221+coredex-source@users.noreply.github.com>
2024-12-19 19:57:05 +0530

    Config screen bump.
    - Bumped fabric api 0.112.1+1.21.4 -> 0.112.2+1.21.4
    - Massively changed readme
    - Changed fps settings in config
    - Implemented config options in custom screen to ease **optional** mod menu requirement.

--- README.md
+++ README.md
@@ -1,12 +1,10 @@
-# ForceGL2.0
-- A Minecraft mod for [Fabric](https://fabricmc.net/), [Forge](https://files.minecraftforge.net/net/minecraftforge/forge/), [Quilt](https://quiltmc.org/) & [NeoForge](https://neoforged.net) that forces the game to use a specified OpenGL version.
+# ForceGL_AdaptiveRenderScaling(ForceGL_ARS)
+- A Minecraft mod for [Fabric](https://fabricmc.net/), [Forge](https://files.minecraftforge.net/net/minecraftforge/forge/), [Quilt](https://quiltmc.org/) & [NeoForge](https://neoforged.net) that includes multiple render functionalities.
 - You can download it using [CurseForge](https://www.curseforge.com/minecraft/mc-mods/forcegl2-0-remapped), [Modrinth](https://modrinth.com/mod/forcegl2.0-remapped).
 
-## Mod does not load up on forge or neoforge?
-The loaders take early control of the loading screen, to prevent that from happening-
-- Launch the game once and let it crash.
-- Navigate to .minecraft/config folder, edit the fml.toml file and change **earlyWindowControl = true** to **earlyWindowControl = false**.
-- Load the game again with the mod in the mods folder and it should load up normally.
+## Features-
+- OpenGL version switch.
+- AdaptiveChunkScaling - Changes render distance based on your avg. frame rate.
 
 ## Fixes-
 - Fixes driver crash with old GPUs caused by Java exception while loading shaders using Iris or Oculus. (Including the one's which can run Minecraft without the mod.)
@@ -16,10 +14,11 @@ The loaders take early control of the loading screen, to prevent that from happe
 - Since Minecraft 1.17, Mojang changed the required OpenGL version to 3.2.  
 - This means that players with an old graphic card that doesn't support OpenGL 3.2 can no longer play the game.  
 - This Minecraft mod makes the game work again for those players by reverting the OpenGL GLFW hints to how it was in Minecraft 1.16.
+- Changing render distance dynamically can suit multiple environments where players either need to use high render distances or are limited by performance but would still like to see at a considerate render distance.
 
 ## But does it work?
 - As far as I tested, there have been no issues. However, you must remember that this mod forces the game to run on an unsupported OpenGL version. There may be unexpected issues with other mods or resource packs.  
-- **While it is fine to use this mod to run newer versions of Minecraft, please do consider upgrading to a newer graphic card as soon as possible.**
+- If an issue like the one stated above appears simply turn off the GL override and just use the **ARS** functionality of the mod.
 
 ## I found an issue!
 - Due to the statement above (that you are running the game on an unsupported OpenGL), I cannot provide extreme support for issues you may experience in this mod. This mod simply changes the OpenGL version and does not apply any other changes to the game's code. I will still provide as much support as I can.
@@ -31,5 +30,5 @@ The loaders take early control of the loading screen, to prevent that from happe
 ```
 - Use the build from build/libs.
 
-## Orignal mod
+## Orignal mod credits (GL switch logic only.)
 - Orignal mod by KabanFriends - [Github](https://github.com/KabanFriends/ForceGL20) [Modrinth](https://modrinth.com/mod/forcegl20)
--- src/main/java/io/github/coredex/forcegl20/config/ForceGLOptionsScreen.java
+++ src/main/java/io/github/coredex/forcegl20/config/ForceGLOptionsScreen.java
@@ -9,60 +9,204 @@ import net.minecraft.text.Text;
 public class ForceGLOptionsScreen extends Screen {
     private final Screen parent;
     private int openGLVersion = ForceGL20Config.CONFIG.instance().contextVersionMajor;
-    private boolean modEnabled = ForceGL20Config.CONFIG.instance().modEnabled; 
+    private boolean modEnabled = ForceGL20Config.CONFIG.instance().modEnabled;
+    private boolean irisIFOverride = ForceGL20Config.CONFIG.instance().irisIFOverride;
+
+    // Do not touch the line below as it's required in a different class.
     public static boolean ARScalingEnabled = ForceGL20Config.CONFIG.instance().adaptiveRenderScalingEnabled;
+    private int minFpsThreshold = ForceGL20Config.CONFIG.instance().minFpsThreshold;
+    private int maxFpsThreshold = ForceGL20Config.CONFIG.instance().maxFpsThreshold;
+    private int minRenderDistance = ForceGL20Config.CONFIG.instance().minRenderDistance;
+    private int maxRenderDistance = ForceGL20Config.CONFIG.instance().maxRenderDistance;
+    private int defaultRenderDistance = ForceGL20Config.CONFIG.instance().defaultRenderDistance;
+    private int checkInterval = ForceGL20Config.CONFIG.instance().checkInterval;
+    private int updateInterval = ForceGL20Config.CONFIG.instance().updateInterval;
+
+    private boolean showOpenGLConfig = true; // Track which config is currently shown
 
     public ForceGLOptionsScreen(Screen parent, Text title) {
         super(title);
-        this.parent = parent; 
+        this.parent = parent;
     }
 
     @Override
     protected void init() {
-        ButtonWidget toggleModButton = ButtonWidget.builder(
-            Text.literal("Enable GL Mod: " + (modEnabled ? "ON" : "OFF")),
-            button -> {
-                modEnabled = !modEnabled;
-                button.setMessage(Text.literal("Enable GL Mod: " + (modEnabled ? "ON" : "OFF")));
-            }
-        ).dimensions(
-            this.width / 2 - 100,
-            this.height / 2 - 60,
-            200,
-            20
-        ).build();
+        this.clearChildren();
 
-        this.addDrawableChild(toggleModButton);
+        int topLeftY = 30;
+        int buttonWidth = 150;
+        int buttonHeight = 20;
+        int spacing = 25;
 
-        this.addDrawableChild(new SliderWidget(this.width / 2 - 100, this.height / 2 - 20, 200, 20,
-                Text.literal("OpenGL Version: " + openGLVersion), openGLVersion / 4.0) {
-            @Override
-            protected void updateMessage() {
-                this.setMessage(Text.literal("OpenGL Version: " + openGLVersion));
-            }
+        this.addDrawableChild(ButtonWidget.builder(
+                Text.literal("OpenGL Config"),
+                button -> {
+                    showOpenGLConfig = true;
+                    this.init();
+                }
+        ).dimensions(10, topLeftY, buttonWidth, buttonHeight).build());
 
-            @Override
-            protected void applyValue() {
-                openGLVersion = (int) Math.round(this.value * 4);
-            }
-        });
+        this.addDrawableChild(ButtonWidget.builder(
+                Text.literal("ARS Config"),
+                button -> {
+                    showOpenGLConfig = false;
+                    this.init(); // Reinitialize to refresh the buttons
+                }
+        ).dimensions(20 + buttonWidth, topLeftY, buttonWidth, buttonHeight).build());
 
+        int centerX = this.width / 2 - 100;
+        int centerY = this.height / 2 - 60;
+
+        if (showOpenGLConfig) {
+            // OpenGL Config buttons
+            this.addDrawableChild(ButtonWidget.builder(
+                    Text.literal("Enable GL Mod: " + (modEnabled ? "ON" : "OFF")),
+                    button -> {
+                        modEnabled = !modEnabled;
+                        button.setMessage(Text.literal("Enable GL Mod: " + (modEnabled ? "ON" : "OFF")));
+                    }
+            ).dimensions(centerX, centerY, 200, buttonHeight).build());
+
+            this.addDrawableChild(new SliderWidget(centerX, centerY + spacing, 200, buttonHeight,
+                    Text.literal("OpenGL Version: " + openGLVersion), openGLVersion / 4.0) {
+                @Override
+                protected void updateMessage() {
+                    this.setMessage(Text.literal("OpenGL Version: " + openGLVersion));
+                }
+
+                @Override
+                protected void applyValue() {
+                    openGLVersion = (int) Math.round(this.value * 4);
+                }
+            });
+
+            this.addDrawableChild(ButtonWidget.builder(
+                    Text.literal("Iris IF Override: " + (irisIFOverride ? "ON" : "OFF")),
+                    button -> {
+                        irisIFOverride = !irisIFOverride;
+                        button.setMessage(Text.literal("Iris IF Override: " + (irisIFOverride ? "ON" : "OFF")));
+                    }
+            ).dimensions(centerX, centerY + 2 * spacing, 200, buttonHeight).build());
+        } else {
+            this.addDrawableChild(new SliderWidget(centerX, centerY, 200, buttonHeight,
+                    Text.literal("Min FPS Threshold: " + minFpsThreshold), (minFpsThreshold - 20) / 100.0) {
+                @Override
+                protected void updateMessage() {
+                    this.setMessage(Text.literal("Min FPS Threshold: " + minFpsThreshold));
+                }
+
+                @Override
+                protected void applyValue() {
+                    minFpsThreshold = 20 + (int) Math.round(this.value * 100 / 5) * 5;
+                }
+            });
+
+            this.addDrawableChild(new SliderWidget(centerX, centerY + spacing, 200, buttonHeight,
+                    Text.literal("Max FPS Threshold: " + maxFpsThreshold), (maxFpsThreshold - 30) / 330.0) {
+                @Override
+                protected void updateMessage() {
+                    this.setMessage(Text.literal("Max FPS Threshold: " + maxFpsThreshold));
+                }
+
+                @Override
+                protected void applyValue() {
+                    maxFpsThreshold = 30 + (int) Math.round(this.value * 330 / 5) * 5;
+                }
+            });
+
+            this.addDrawableChild(new SliderWidget(centerX, centerY + 2 * spacing, 200, buttonHeight,
+                    Text.literal("Min Render Distance: " + minRenderDistance), (minRenderDistance - 2) / 18.0) {
+                @Override
+                protected void updateMessage() {
+                    this.setMessage(Text.literal("Min Render Distance: " + minRenderDistance));
+                }
+
+                @Override
+                protected void applyValue() {
+                    minRenderDistance = 2 + (int) Math.round(this.value * 18);
+                }
+            });
+
+            this.addDrawableChild(new SliderWidget(centerX, centerY + 3 * spacing, 200, buttonHeight,
+                    Text.literal("Max Render Distance: " + maxRenderDistance), (maxRenderDistance - 4) / 28.0) {
+                @Override
+                protected void updateMessage() {
+                    this.setMessage(Text.literal("Max Render Distance: " + maxRenderDistance));
+                }
+
+                @Override
+                protected void applyValue() {
+                    maxRenderDistance = 4 + (int) Math.round(this.value * 28);
+                }
+            });
+
+            this.addDrawableChild(new SliderWidget(centerX, centerY + 4 * spacing, 200, buttonHeight,
+                    Text.literal("Default Render Distance: " + defaultRenderDistance), (defaultRenderDistance - 4) / 28.0) {
+                @Override
+                protected void updateMessage() {
+                    this.setMessage(Text.literal("Default Render Distance: " + defaultRenderDistance));
+                }
+
+                @Override
+                protected void applyValue() {
+                    defaultRenderDistance = 4 + (int) Math.round(this.value * 28);
+                }
+            });
+
+            this.addDrawableChild(new SliderWidget(centerX, centerY + 5 * spacing, 200, buttonHeight,
+                    Text.literal("Check Interval: " + checkInterval + " ms"), (checkInterval - 500) / 1500.0) {
+                @Override
+                protected void updateMessage() {
+                    this.setMessage(Text.literal("Check Interval: " + checkInterval + " ms"));
+                }
+
+                @Override
+                protected void applyValue() {
+                    checkInterval = 500 + (int) Math.round(this.value * 1500 / 500) * 500;
+                }
+            });
+
+            this.addDrawableChild(new SliderWidget(centerX, centerY + 6 * spacing, 200, buttonHeight,
+                    Text.literal("Update Interval: " + updateInterval + " ms"), (updateInterval - 1000) / 29000.0) {
+                @Override
+                protected void updateMessage() {
+                    this.setMessage(Text.literal("Update Interval: " + updateInterval + " ms"));
+                }
+
+                @Override
+                protected void applyValue() {
+                    updateInterval = 1000 + (int) Math.round(this.value * 29000 / 1000) * 1000;
+                }
+            });
+        }
+
+        // Common buttons (Save and Back)
         this.addDrawableChild(ButtonWidget.builder(
                 Text.literal("Save"),
                 button -> {
                     ForceGL20Config.CONFIG.instance().contextVersionMajor = openGLVersion;
                     ForceGL20Config.CONFIG.instance().modEnabled = modEnabled;
+                    ForceGL20Config.CONFIG.instance().irisIFOverride = irisIFOverride;
+                    ForceGL20Config.CONFIG.instance().adaptiveRenderScalingEnabled = ARScalingEnabled;
+                    ForceGL20Config.CONFIG.instance().minFpsThreshold = minFpsThreshold;
+                    ForceGL20Config.CONFIG.instance().maxFpsThreshold = maxFpsThreshold;
+                    ForceGL20Config.CONFIG.instance().minRenderDistance = minRenderDistance;
+                    ForceGL20Config.CONFIG.instance().maxRenderDistance = maxRenderDistance;
+                    ForceGL20Config.CONFIG.instance().defaultRenderDistance = defaultRenderDistance;
+                    ForceGL20Config.CONFIG.instance().checkInterval = checkInterval;
+                    ForceGL20Config.CONFIG.instance().updateInterval = updateInterval;
                     ForceGL20Config.CONFIG.save();
+                    DynamicConfigUpdates.applyDynamicChanges();
                     if (this.client != null) this.client.setScreen(parent);
                 }
-        ).dimensions(this.width / 2 - 155, this.height / 2 + 40, 150, 20).build());
+        ).dimensions(this.width - 325, this.height - 40, buttonWidth, buttonHeight).build());
 
         this.addDrawableChild(ButtonWidget.builder(
                 Text.literal("Back"),
                 button -> {
                     if (this.client != null) this.client.setScreen(parent);
                 }
-        ).dimensions(this.width / 2 + 5, this.height / 2 + 40, 150, 20).build());
+        ).dimensions(this.width - 160, this.height - 40, buttonWidth, buttonHeight).build());
     }
 
     @Override
@@ -70,22 +214,24 @@ public class ForceGLOptionsScreen extends Screen {
         super.render(context, mouseX, mouseY, delta);
         context.drawCenteredTextWithShadow(this.textRenderer, this.title, this.width / 2, 15, 0xFFFFFF);
 
-        String note = "Note: Changing the GL version will require a restart. (Mod default = 2)";
-        context.drawCenteredTextWithShadow(
-            this.textRenderer,
-            Text.literal(note),
-            this.width / 2,
-            this.height / 2 + 5,
-            0xAAAAAA
-        );
+        if (showOpenGLConfig) {
+            String note = "Note: Changing the GL version will require a restart. (Mod default = 2)";
+            context.drawCenteredTextWithShadow(
+                this.textRenderer,
+                Text.literal(note),
+                this.width / 2,
+                this.height / 2 + 20,
+                0xAAAAAA
+            );
 
-        String compatibilityNote = "For best compatibility use 3 or 4 if your graphics card supports it.";
-        context.drawCenteredTextWithShadow(
-            this.textRenderer,
-            Text.literal(compatibilityNote),
-            this.width / 2,
-            this.height / 2 + 25,
-            0xAAAAAA
-        );
+            String compatibilityNote = "For best compatibility use 3 or 4 if your graphics card supports it.";
+            context.drawCenteredTextWithShadow(
+                this.textRenderer,
+                Text.literal(compatibilityNote),
+                this.width / 2,
+                this.height / 2 + 35,
+                0xAAAAAA
+            );
+        }
     }
 }
